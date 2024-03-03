17ea0b74c69c51a81e42113eb7657e291c13c7c6
Pigfection <pigfection@gmail.com>
2024-03-03 13:23:08 +1000

    Initial commit

---
+++ src/main/java/net/pigfection/woodnstone/WoodNStone.java
@@ -0,0 +1,18 @@
+package net.pigfection.woodnstone;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.pigfection.woodnstone.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WoodNStone implements ModInitializer {
+	public static final String MOD_ID = "woodnstone";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/net/pigfection/woodnstone/WoodNStoneClient.java
@@ -0,0 +1,10 @@
+package net.pigfection.woodnstone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WoodNStoneClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
