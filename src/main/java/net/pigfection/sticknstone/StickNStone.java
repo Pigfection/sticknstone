2129a27aed021643ad8870f0e53f334cb8eb7ed5
Pigfection <pigfection@gmail.com>
2024-03-05 09:45:49 +1000

    Changed mod name to Stick n' Stone

---
+++ src/main/java/net/pigfection/sticknstone/StickNStone.java
@@ -0,0 +1,19 @@
+package net.pigfection.sticknstone;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.pigfection.sticknstone.item.ModItemGroups;
+import net.pigfection.sticknstone.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StickNStone implements ModInitializer {
+	public static final String MOD_ID = "sticknstone";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+	}
+}
